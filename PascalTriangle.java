fbed8df0d0be5e35e59df4ccbcd4532d7652af05
KARANNEHRA12 <choudharykaran351@gmail.com>
2023-02-22 18:17:21 +0530

    dp added

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -2,5 +2,17 @@
 // Q: Pascal's triangle 
 
 public class PascalTriangle {
+    public static int[] change(int input[]) {
+        input = new int[5];
+        input[3] = 15;
+        return input;
+    }
+
+    public static void main(String[] args) {
+        String str1 = new String ("I love coding"); 
+    String str2 = "I love coding"; 
+    boolean check=(str1=="I love coding");
+    System.out.println((str1 == str2) + " " + str1.equals(str2)+" " + check); 
+    }
 
 }
