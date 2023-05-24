de4324eca5405f7554054a5dccb0bdb92b0639b8
KARANNEHRA12 <choudharykaran351@gmail.com>
2023-05-24 22:18:55 +0530

    ll added

---
+++ LinkedList/ReverseLL.java
@@ -0,0 +1,16 @@
+//Q:https://leetcode.com/problems/reverse-linked-list/
+ public class ReverseLL {
+//    Definition for singly-linked list
+             public class ListNode {
+   int val;
+    ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+    public ListNode reverse( ListNode head) {
+
+    }
+
+
+}
